0e3434529c169db614eede4ff55dd00d54a86ff4
Fabricio Orihuela <figueorafabricio14@gmail.com>
2025-11-07 00:19:14 -0500

    Implente imagenes el home page

--- src/app/home/testimonials/testimonials.ts
+++ src/app/home/testimonials/testimonials.ts
@@ -10,7 +10,20 @@ import { CommonModule } from '@angular/common';
 })
 export class TestimonialsComponent {
   testimonials = [
-    { name: 'Carlos Pérez', message: 'Excelente atención a mi perro!' },
-    { name: 'Lucía Torres', message: 'Muy confiables y profesionales.' }
+    {
+      name: 'Carlos Pérez',
+      message: 'Excelente atención a mi perro, el equipo fue muy amable y profesional.',
+      image: '/assets/imagen2.jpg'
+    },
+    {
+      name: 'Lucía Torres',
+      message: 'Muy confiables y atentos. Mi gato recibió el mejor cuidado posible.',
+      image: '/assets/imagen3.webp'
+    },
+    {
+      name: 'María Delgado',
+      message: 'Recomiendo totalmente esta clínica. Se nota el amor que tienen por los animales.',
+      image: '/assets/imagen1.jpg'
+    }
   ];
 }
